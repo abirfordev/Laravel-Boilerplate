29b13887844022606b765c615bdb434a85a51243
Abir Das <abirdas422@gmail.com>
2023-10-12 14:57:10 +0600

    admin layout setup done

---
+++ resources/views/backend/admin/layout/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="content-footer footer bg-footer-theme">
+    <div class="container-xxl d-flex flex-wrap justify-content-center py-2 flex-md-row flex-column">
+        <div class="mb-2 mb-md-0">
+            ©{{ date('Y') }}, made by
+            <a href="#" target="_blank" class="footer-link fw-bolder">CSEAA-PU Developer
+                Team</a>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/backend/admin/layout/master.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+
+<html lang="en" class="light-style layout-menu-fixed layout-compact" dir="ltr" data-theme="theme-default"
+    data-assets-path="/assets/" data-template="vertical-menu-template-free">
+
+<head>
+    @include('backend.admin.layout.head')
+</head>
+
+<body>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+        <div class="layout-container">
+
+            <!-- Sidebar -->
+            @include('backend.admin.layout.sidebar')
+            <!-- / Sidebar -->
+
+            <!-- Layout container -->
+            <div class="layout-page">
+
+                <!-- Topbar -->
+                @include('backend.admin.layout.topbar')
+                <!-- / Topbar -->
+
+                <!-- Content wrapper -->
+                <div class="content-wrapper">
+
+                    <!-- Content -->
+                    <div class="container-xxl flex-grow-1 container-p-y">
+                        @yield('content')
+                    </div>
+                    <!-- / Content -->
+
+                    <!-- Footer -->
+                    @include('backend.admin.layout.footer')
+                    <!-- / Footer -->
+
+                    <div class="content-backdrop fade"></div>
+                </div>
+                <!-- Content wrapper -->
+            </div>
+            <!-- / Layout page -->
+        </div>
+
+        <!-- Overlay -->
+        <div class="layout-overlay layout-menu-toggle"></div>
+    </div>
+    <!-- / Layout wrapper -->
+
+    <!-- Footer JS-->
+    @include('backend.admin.layout.footer-js')
+    <!-- / Footer JS-->
+
+</body>
+
+</html>
